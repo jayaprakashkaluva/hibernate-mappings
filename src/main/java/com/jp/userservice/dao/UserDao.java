cdc3eb929c9f8559d25f50979299d3b14dcdd9ce
jayap <kjayaprakash4028@gmail.com>
2019-07-10 13:52:37 -0400

    fix for n+1 select

--- src/main/java/com/jp/userservice/dao/UserDao.java
+++ src/main/java/com/jp/userservice/dao/UserDao.java
@@ -1,9 +1,16 @@
 package com.jp.userservice.dao;
 
 import com.jp.userservice.model.User;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao extends CrudRepository<User,Long>{
+	
+	@Query("SELECT p FROM User p LEFT JOIN FETCH p.billingAddress LEFT JOIN FETCH p.shippingAddress") 
+	public List<User> findAll();
 }
